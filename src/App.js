2b1e6083ee35a71e6e9052a8eb7991c4a78147b8
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-09-17 15:19:25 -0500

    added cowsay

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const cowsay = require('cowsay');
+
 class App extends Component {
   constructor(props){
     super(props)
