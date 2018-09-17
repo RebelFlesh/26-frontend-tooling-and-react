73e0c201f7cd35c5dd16132307f58fc5cde8f1f6
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-09-17 15:17:35 -0500

    scafolded app content

--- src/App.js
+++ src/App.js
@@ -12,13 +12,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={this.updateContent}>Click Me!</button>
+        <pre>{this.state.content}</pre>
       </div>
     );
   }
