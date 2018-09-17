a3c1b795458a8a776d02b524f8c20f46c3c170e7
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-09-17 15:16:37 -0500

    added App ctor

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    
+    this.state = {
+      content: cowsay.say({text : "hello world"}),
+    }
   render() {
     return (
       <div className="App">
